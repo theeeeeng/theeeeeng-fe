520a444e556fcd82c09680cf4dce44dd9faab650
theeeeeng <theeeeeng@gmail.com>
2023-02-25 22:25:19 +0900

    feature: 로그인 페이지 UI, 밸리데이션 체크 작업

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,47 @@
 import Link from 'next/link';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, {useState, useRef, useEffect, ChangeEvent} from 'react';
 import styled from 'styled-components';
+import { LoginError, LoginForm } from '../types/login';
 
 const LoginPage: NextPage = () => {
+  const [form, setForm] = useState<LoginForm>({
+    id: '',
+    password: ''
+  });
+  const [error, setError] = useState<LoginError>({
+    id: undefined,
+    password: undefined
+  });
+
+  const handleChange = (type: 'id' | 'password', event: ChangeEvent<HTMLInputElement>) => {
+    if (type === 'id') {
+      const id = event.target.value;
+      setForm({id, password: form.password});
+    } else if (type === 'password') {
+      const password = event.target.value;
+      setForm({id: form.id, password});
+    }
+
+  };
+
+  const handleBlur = (type: 'id' | 'password') => {
+    if (type === 'id') {
+      setError({id: check('id') ? '' : '올바른 아이디 형식으로 입력해주세요.', password: error.password});
+    } else if (type === 'password') {
+      setError({id: error.id, password: check('password') ? '' : '올바른 비밀번호 형식으로 입력해주세요.'});
+    }
+  };
+
+  const check = (type: 'id' | 'password'): boolean => {
+    if (type === 'id') {
+      return (form.id.length > 0 && /^[A-za-z0-9]{5,30}/.test(form.id));
+    } else if (type === 'password') {
+      return (form.password.length > 0 && /^[A-za-z0-9]{8,30}$/.test(form.password));
+    }
+    return false;
+  };
+
   return (
     <>
       <Header>
@@ -15,11 +53,17 @@ const LoginPage: NextPage = () => {
         </Link>
       </Header>
       <Form>
-        <div>아이디</div>
-        <TextInput type='text' />
-        <div>비밀번호</div>
-        <TextInput type='password' />
-        <LoginButton disabled>로그인</LoginButton>
+        <FieldTitle>아이디</FieldTitle>
+        <TextInput type='text' minLength={5} maxLength={30} value={form.id} error={!!error.id} onChange={(e) => handleChange('id', e)} onBlur={() => handleBlur('id')}/>
+        {
+          !!error.id && <ErrorText>{error.id}</ErrorText>
+        }
+        <FieldTitle>비밀번호</FieldTitle>
+        <TextInput type='password' minLength={8} maxLength={30} value={form.password} error={!!error.password} onChange={(e) => handleChange('password', e)} onBlur={() => handleBlur('password')}/>
+        {
+          !!error.password && <ErrorText>{error.password}</ErrorText>
+        }
+        <LoginButton disabled={error.id === undefined || !!error.id || error.password === undefined || !!error.password}>로그인</LoginButton>
       </Form>
     </>
   );
@@ -44,9 +88,19 @@ const Form = styled.div`
   margin-top: 40px;
   padding: 0 20px 40px;
 `;
+const FieldTitle = styled.div`
+  margin-top: 8px;
+  font-weight: 700;
+  font-size: 13px;
+  color: #6C6C7D;
+`;
 
-const TextInput = styled.input`
-  border: 1px solid #000;
+const TextInput = styled.input<{error: boolean}>`
+  margin-top: 8px;
+  padding: 16px;
+  border-radius: 12px;
+
+  background: ${prop => prop.error ? '#FDEDEE' : '#F7F7FA'};
 `;
 
 const LoginButton = styled.button`
@@ -59,4 +113,11 @@ const LoginButton = styled.button`
   &:disabled {
     background-color: #e2e2ea;
   }
+`;
+
+const ErrorText = styled.p`
+  margin-top: 8px;
+  font-weight: 400;
+  font-size: 13px;
+  color: #ED4E5C;
 `;
